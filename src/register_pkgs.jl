a99ae955ceb8d7d6a041868e86a086716b80a160
Brian Cheung <bcheung.dev@gmail.com>
2020-04-23 18:14:22 -0500

    Make the manifest optional

--- src/register_pkgs.jl
+++ src/register_pkgs.jl
@@ -3,10 +3,10 @@ Pkg.instantiate()
 
 import LocalRegistry
 
-function register_pkg(pkg_path, registry_path, manifest_path)
+function register_pkg(pkg_path, registry_path, manifest_path=nothing)
   @info "ARGS" pkg_path registry_path
   repo = "$pkg_path/.git"
   LocalRegistry.register(pkg_path, registry_path, manifest_path, repo=repo)
 end
 
-register_pkg(ARGS[1], ARGS[2], ARGS[3])
+register_pkg(ARGS...)
